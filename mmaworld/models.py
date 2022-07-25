91d649d03af08958499b58d730378c54f130d20e
Scott Atkinson <scott.atkin79@gmail.com>
2022-07-25 19:54:34 +0000

    update order model

--- mmaworld/models.py
+++ mmaworld/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class UserProfile(models.Model):
@@ -6,7 +7,10 @@ class UserProfile(models.Model):
 
 
 class Item(models.Model):
-    pass
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 
 class OrderItem(models.Model):
@@ -14,7 +18,16 @@ class OrderItem(models.Model):
 
 
 class Order(models.Model):
-    pass
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
 
 class Address(models.Model):
     pass
